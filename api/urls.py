309cd2e042bc5f6bb50e00a73ee91fed4dfdd72f
Amar Dongardive <amardongardive8@gmail.com>
2026-01-21 16:20:19 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
 router = DefaultRouter()
-router.register('bookss', BookViewSet)
+router.register('books', BookViewSet)
 
 urlpatterns = router.urls
